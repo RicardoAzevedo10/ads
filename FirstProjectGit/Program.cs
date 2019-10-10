3a5a68c1cab07edd01637fe972c99247daa3e96d
Ricardo Hilário Azevedo <A035100@ipmaia.pt>
2019-10-10 16:36:03 +0100

    commit inicial

---
+++ FirstProjectGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstProjectGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
